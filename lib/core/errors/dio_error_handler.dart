c1e7edf9e6bdf1358256f8ba27109d6da8d2059b
Amaar Abd <abdamaar64@gmail.com>
2026-03-22 20:47:32 +0300

    feat: add error handling function fpr api requests

---
+++ lib/core/errors/dio_error_handler.dart
@@ -0,0 +1,30 @@
+import 'package:dio/dio.dart';
+
+class DioErrorHandler {
+  static String dioHande(DioException e) {
+    switch (e.type) {
+      case DioExceptionType.connectionTimeout:
+        return 'Connection timeout';
+      case DioExceptionType.receiveTimeout:
+        return 'Receive timeout';
+      case DioExceptionType.connectionError:
+        return 'Please check your network connection.';
+      case DioExceptionType.badResponse:
+        return _handleStatusCode(e.response?.statusCode);
+
+      default:
+        return 'Unecpected error';
+    }
+  }
+
+  static String _handleStatusCode(int? statusCode) {
+    switch (statusCode) {
+      case 404:
+        return 'Resource not found (404). Please check the URL or request data.';
+      case 500:
+        return 'Internal server error (500). Please try again later.';
+      default:
+        return 'Something went wrong. Status code: $statusCode';
+    }
+  }
+}
