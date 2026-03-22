d2a6c95a490077eb38c9552852c643a7aa7b85d7
Amaar Abd <abdamaar64@gmail.com>
2026-03-22 20:06:50 +0300

    feat: implement API remote data source for fetching data

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,3 +1,6 @@
 class ApiConstants {
-  
+  static const String baseUrl = 'https://newsapi.org/v2/';
+  static const String apiKey = '071e9e53d1cb4a7180c22403a557794e';
+  static const String topHeadlines = 'top-headlines';
+  static const String everything = 'everything';
 }
