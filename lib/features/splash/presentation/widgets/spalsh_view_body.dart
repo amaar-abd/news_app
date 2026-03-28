13d1adfadd355b383ae57ef78e8f0684165319c7
Amaar Abd <abdamaar64@gmail.com>
2026-03-28 15:16:39 +0300

    UI: update splash screen logo

--- lib/features/splash/presentation/widgets/spalsh_view_body.dart
+++ lib/features/splash/presentation/widgets/spalsh_view_body.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/core/constants/app_colors.dart';
 import 'package:news_app/core/routing/app_routes.dart';
 import 'package:news_app/core/utils/app_iamges.dart';
 
@@ -19,61 +18,8 @@ class _SpalshViewBodyState extends State<SpalshViewBody> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          DecoratedBox(
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.transparent,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey,
-                  blurRadius: 10,
-                  spreadRadius: 2,
-                  offset: const Offset(0, 15),
-                ),
-              ],
-            ),
-            child: Image.asset(
-              Assets.assetsImagesLogo,
-              height: 170,
-              width: 170,
-            ),
-          ),
-          SizedBox(height: 40),
-          Text.rich(
-            TextSpan(
-              children: [
-                TextSpan(
-                  text: 'NEWS ',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-                TextSpan(
-                  text: 'PRIMY',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: AppColors.primaryColors,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Text(
-            'YOUR DAILY PERSPECTIVE',
-            style: Theme.of(
-              context,
-            ).textTheme.bodyMedium?.copyWith(letterSpacing: 1.8),
-          ),
-        ],
-      ),
+    return Center(
+      child: Image.asset(Assets.assetsImagesLogo, height: 350, width: 350),
     );
   }
 
